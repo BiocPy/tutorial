896747645a706633aafa18c1a3cdcde139603dd7
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-02-20 09:02:05 -0800

    test the R workflow

--- r_requirements.r
+++ r_requirements.r
@@ -1,5 +1,6 @@
-# install.packages(c("BiocManager", "devtools"), repos='http://cran.us.r-project.org')
-BiocManager::install(version = "3.18")
+install.packages(c("BiocManager", "devtools"), repos='http://cran.us.r-project.org')
+BiocManager::install(version = "3.18", ask=FALSE)
 
 # install alabaster
-BiocManager::install(c("alabaster"))
+BiocManager::install(c("alabaster", "scRNAseq"))
+
