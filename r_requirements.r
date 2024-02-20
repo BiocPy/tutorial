9dd40a13812d7179ebc4d791c83cf574aaf05d7b
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-02-20 07:41:34 -0800

    include R step in gh actions

---
+++ r_requirements.r
@@ -0,0 +1,5 @@
+install.packages(c()"BiocManager", "devtools"), repos='http://cran.us.r-project.org')
+BiocManager::install(version = "3.18")
+
+# install alabaster
+BiocManager::install(c("alabaster"))
